e985b4f8f495fcb422c5c552de5ec1f176e41705
Vlad <morpexx08@gmail.com>
2024-03-05 14:43:10 +0200

    Added frontend layer

---
+++ PasteBinClone/PasteBinClone.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PasteBinClone.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
