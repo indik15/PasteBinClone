1274c5722b8ef776e6c28bdd2ca5e7cac4a5ec83
Vlad <morpexx08@gmail.com>
2024-03-18 16:53:14 +0200

    Added Edit profile view

--- PasteBinClone/PasteBinClone.Web/Controllers/ProfileController.cs
+++ PasteBinClone/PasteBinClone.Web/Controllers/ProfileController.cs
@@ -8,5 +8,10 @@ namespace PasteBinClone.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
     }
 }
