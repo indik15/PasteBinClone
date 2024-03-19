fbad28699c9c06d8cdc10cee48d57eedc8608585
Vlad <morpexx08@gmail.com>
2024-03-19 17:30:02 +0200

    Added Delete view for Category controller

--- PasteBinClone/PasteBinClone.Web/Controllers/CategoryController.cs
+++ PasteBinClone/PasteBinClone.Web/Controllers/CategoryController.cs
@@ -13,5 +13,10 @@ namespace PasteBinClone.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
     }
 }
