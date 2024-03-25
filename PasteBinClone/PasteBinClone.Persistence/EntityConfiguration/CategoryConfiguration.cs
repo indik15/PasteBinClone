033383eb20af8d80c73976f1348ad5d9b082782a
Vlad <morpexx08@gmail.com>
2024-03-25 17:45:53 +0200

    Added NuGet packages and set configurations for Category

---
+++ PasteBinClone/PasteBinClone.Persistence/EntityConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PasteBinClone.Domain.Models;
+
+namespace PasteBinClone.Persistence.EntityConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id).IsUnique();
+            builder.Property(c => c.CategoryName).HasMaxLength(50).IsRequired();
+        }
+    }
+}
