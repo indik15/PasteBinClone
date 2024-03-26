5e43b82e5ba4abfdbc895c58b17de17aeee9691b
Vlad <morpexx08@gmail.com>
2024-03-26 17:24:34 +0200

    Added DbContext class

---
+++ PasteBinClone/PasteBinClone.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasteBinClone.Domain.Models;
+using PasteBinClone.Persistence.EntityConfiguration;
+
+namespace PasteBinClone.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
