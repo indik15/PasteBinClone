a472390608803e9c10486f14b04b40f85a5242ff
Vlad <morpexx08@gmail.com>
2024-03-29 16:46:07 +0200

    Added VIew Model for Category

---
+++ PasteBinClone/PasteBinClone.Application/ViewModels/CategoryVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.ViewModels
+{
+    public record CategoryVM(
+    int id,
+    string CategoryName);
+}
