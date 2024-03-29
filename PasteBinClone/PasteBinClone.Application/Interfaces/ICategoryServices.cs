4ca5b14c2d6d7e84a79064d662f37644910154a4
Vlad <morpexx08@gmail.com>
2024-03-29 16:39:18 +0200

    Added interface for CategoryServices

---
+++ PasteBinClone/PasteBinClone.Application/Interfaces/ICategoryServices.cs
@@ -0,0 +1,13 @@
+﻿using PasteBinClone.Application.Dto;
+
+namespace PasteBinClone.Application.Interfaces
+{
+    public interface ICategoryServices
+    {
+        Task<CategoryDto> GetCategoryByID(int id);
+        Task<IEnumerable<CategoryDto>> GetAllCategory();
+        Task<bool> CreateCategory(CategoryDto category);
+        Task<bool> DeleteCategory(int id);
+        Task<bool> UpdateCategory(CategoryDto categoryDto);
+    }
+}
