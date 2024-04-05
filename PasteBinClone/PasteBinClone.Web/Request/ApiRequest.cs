18b1af2caedee77076a8448479c73db74313d608
Vlad <morpexx08@gmail.com>
2024-04-05 15:47:22 +0300

    Added request settings

---
+++ PasteBinClone/PasteBinClone.Web/Request/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿
+using static PasteBinClone.Web.Request.Settings;
+
+namespace PasteBinClone.Web.Request
+{
+    public class ApiRequest
+    {
+        public ApiMethod ApiMethod { get; set; } = ApiMethod.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
