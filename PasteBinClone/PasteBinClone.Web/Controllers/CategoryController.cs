3f6fa27d792f26993863d46f131376aba390006c
Vlad <morpexx08@gmail.com>
2024-04-06 14:28:01 +0300

    Added Create, Delete, Edit methods for category

--- PasteBinClone/PasteBinClone.Web/Controllers/CategoryController.cs
+++ PasteBinClone/PasteBinClone.Web/Controllers/CategoryController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PasteBinClone.Web.Interfaces;
 using PasteBinClone.Web.Models.ViewModel;
 
@@ -21,14 +20,71 @@ namespace PasteBinClone.Web.Controllers
             return View(categoryVM);
         }
 
-        public IActionResult Create()
+        //Get-Create
+        [HttpGet]
+        public async Task<IActionResult> Create()
         {
             return View();
         }
 
-        public IActionResult Delete()
+        //Post-Create
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryVM categoryVM)
         {
-            return View();
+            await _categoryService.Post<CategoryVM>(categoryVM);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get-Edit
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var result = await _categoryService.GetById<CategoryVM>(id);
+
+            if(result != null)
+            {
+                return View(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        //Post-Edit
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryVM categoryVM)
+        {
+            await _categoryService.Put<CategoryVM>(categoryVM);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get-Delete
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _categoryService.GetById<CategoryVM>(id);
+
+            if (result != null)
+            {
+                return View(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        //Post-Delete
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            _categoryService.Delete<CategoryVM>(id);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
