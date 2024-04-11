147b14daab79a8f235fb7a14682deb77d474894d
Vlad <morpexx08@gmail.com>
2024-04-11 16:26:10 +0300

    Added settings validation for CategoryDto

---
+++ PasteBinClone/PasteBinClone.Application/Dto/Validations/CategoryDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Dto.Validations
+{
+    public sealed class CategoryDtoValidator : AbstractValidator<CategoryDto>
+    {
+        public CategoryDtoValidator()
+        {
+            RuleFor(u => u.CategoryName).NotEmpty();
+        }
+    }
+}
