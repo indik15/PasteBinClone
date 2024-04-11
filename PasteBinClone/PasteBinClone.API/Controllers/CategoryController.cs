7683626b0149977742ac0f22d547528a2568ca80
Vlad <morpexx08@gmail.com>
2024-04-11 17:40:22 +0300

    Added try/catch for all methods

--- PasteBinClone/PasteBinClone.API/Controllers/CategoryController.cs
+++ PasteBinClone/PasteBinClone.API/Controllers/CategoryController.cs
@@ -5,6 +5,8 @@ using PasteBinClone.API.Response;
 using PasteBinClone.Application.Dto;
 using PasteBinClone.Application.Interfaces;
 using PasteBinClone.Application.ViewModels;
+using Serilog;
+using System.Reflection;
 
 namespace PasteBinClone.API.Controllers
 {
@@ -31,41 +33,61 @@ namespace PasteBinClone.API.Controllers
         [HttpGet]
         public async Task<ActionResult<ResponseAPI>> GetAll()
         {
-            IEnumerable<CategoryDto> categoryDtoList = await _categoryServices.GetAllCategory();
-
-            if (categoryDtoList == null)
+            try
             {
+                IEnumerable<CategoryDto> categoryDtoList = await _categoryServices.GetAllCategory();
+
+                if (categoryDtoList == null)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+                else
+                {
+                    var categoryVM = _mapper.Map<IEnumerable<CategoryVM>>(categoryDtoList);
+                    _response.Data = categoryVM;
+
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while processing GetAll() request.");
                 _response.IsSuccess = false;
 
                 return NotFound(_response);
-            }
-            else
-            {
-                var categoryVM = _mapper.Map<IEnumerable<CategoryVM>>(categoryDtoList);
-                _response.Data = categoryVM;
-
-                return Ok(_response);
             }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<ResponseAPI>> Get(int id)
         {
-            CategoryDto categoryDto = await _categoryServices.GetCategoryByID(id);
-
-
-            if (categoryDto == null)
+            try
             {
+                CategoryDto categoryDto = await _categoryServices.GetCategoryByID(id);
+
+
+                if (categoryDto == null)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+                else
+                {
+                    var categoryVM = _mapper.Map<CategoryVM>(categoryDto);
+                    _response.Data = categoryVM;
+
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while processing GetCategoryByID() request.");
                 _response.IsSuccess = false;
 
                 return NotFound(_response);
-            }
-            else
-            {
-                var categoryVM = _mapper.Map<CategoryVM>(categoryDto);
-                _response.Data = categoryVM;
-
-                return Ok(_response);
             }
 
         }
@@ -73,69 +95,100 @@ namespace PasteBinClone.API.Controllers
         [HttpPost]
         public async Task<ActionResult<ResponseAPI>> Post([FromBody] CategoryDto categoryDto)
         {
-            var valid = _validator.Validate(categoryDto);
-
-            if (!valid.IsValid)
+            try
             {
+                var valid = _validator.Validate(categoryDto);
+
+                if (!valid.IsValid)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+
+                bool result = await _categoryServices.CreateCategory(categoryDto);
+
+                if (!result)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+                else
+                {
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error(ex, "An error occurred while processing Post() request.");
                 _response.IsSuccess = false;
 
                 return NotFound(_response);
-            }
-
-            bool result = await _categoryServices.CreateCategory(categoryDto);
-
-            if (!result)
-            {
-                _response.IsSuccess = false;
-
-                return NotFound(_response);
-            }
-            else
-            {
-                return Ok(_response);
             }
         }
 
         [HttpPut]
         public async Task<ActionResult<ResponseAPI>> Put([FromBody] CategoryDto categoryDto)
         {
-            var valid = _validator.Validate(categoryDto);
-
-            if (!valid.IsValid)
+            try
             {
+                var valid = _validator.Validate(categoryDto);
+
+                if (!valid.IsValid)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+
+                bool result = await _categoryServices.UpdateCategory(categoryDto);
+
+                if (!result)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+                else
+                {
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while processing Put() request.");
                 _response.IsSuccess = false;
 
                 return NotFound(_response);
-            }
-
-            bool result = await _categoryServices.UpdateCategory(categoryDto);
-
-            if (!result)
-            {
-                _response.IsSuccess = false;
-
-                return NotFound(_response);
-            }
-            else
-            {
-                return Ok(_response);
             }
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<ResponseAPI>> Delete(int id)
         {
-            bool result = await _categoryServices.DeleteCategory(id);
-
-            if (!result)
+            try
             {
+                bool result = await _categoryServices.DeleteCategory(id);
+
+                if (!result)
+                {
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+                else
+                {
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while processing Delete() request.");
                 _response.IsSuccess = false;
 
                 return NotFound(_response);
-            }
-            else
-            {
-                return Ok(_response);
             }
         }
 
