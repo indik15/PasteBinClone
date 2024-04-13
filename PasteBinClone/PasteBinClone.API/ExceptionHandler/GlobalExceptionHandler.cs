420c029f7ef0d443415954d24d2a1ccf01667122
Vlad <morpexx08@gmail.com>
2024-04-13 16:52:44 +0300

    Implemented IExceptionHandler interface for error handling

---
+++ PasteBinClone/PasteBinClone.API/ExceptionHandler/GlobalExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System.Net;
+
+namespace PasteBinClone.API.ExceptionHandler
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext, 
+            Exception exception, 
+            CancellationToken cancellationToken)
+        {
+            Log.Error("An error occurred while processing the request: {Message}",
+                exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = (int)HttpStatusCode.NotFound,
+                Type = exception.GetType().Name,
+            };
+
+            await httpContext
+                .Response
+                .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
