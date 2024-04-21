5242de8c3bf1be94e9d3fcc1939b54f3e7e0d702
Vlad <morpexx08@gmail.com>
2024-04-21 17:15:08 +0300

    Added configuration for InMemory database

---
+++ PasteBinClone/PasteBinClone.Tests/AppDbContextInMemory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasteBinClone.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Tests
+{
+    public static class AppDbContextInMemory
+    {
+        public static ApplicationDbContext GetContextInMemory()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TestDb");
+            var context = new ApplicationDbContext(dbContextOptions.Options);
+
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        public static void Destroy(ApplicationDbContext dbContext)
+        {
+            dbContext.Database.EnsureDeleted();
+            dbContext.Dispose();
+        }
+    }
+}
