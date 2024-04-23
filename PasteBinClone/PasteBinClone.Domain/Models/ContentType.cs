b0ac6312dc623b35e12e3998d158b947ea34a4a3
Vlad <morpexx08@gmail.com>
2024-04-23 18:05:23 +0300

    Added ContentType model

---
+++ PasteBinClone/PasteBinClone.Domain/Models/ContentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Domain.Models
+{
+    //Filter class: Type
+    public class ContentType
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
