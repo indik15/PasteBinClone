1811540ae2e791359dfc252f3136084723d2aea0
Vlad <morpexx08@gmail.com>
2024-04-23 19:32:36 +0300

    Added validation rules for ContentType

---
+++ PasteBinClone/PasteBinClone.Application/Dto/Validations/ContentTypeDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Dto.Validations
+{
+    public sealed class ContentTypeDtoValidator : AbstractValidator<ContentTypeDto>
+    {
+        public ContentTypeDtoValidator()
+        {
+            RuleFor(u => u.TypeName).NotNull()
+                .MaximumLength(50);
+        }
+    }
+}
