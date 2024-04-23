1e5a2da8aa41a8c4bc44ce070af55cec745d9f5b
Vlad <morpexx08@gmail.com>
2024-04-23 19:31:57 +0300

    Added ContentTypeVM class

---
+++ PasteBinClone/PasteBinClone.Application/ViewModels/ContentTypeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.ViewModels
+{
+    public class ContentTypeVM
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
