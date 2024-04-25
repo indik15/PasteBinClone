dc74cc3763dda0370f32187b3a439aca819a8e21
Vlad <morpexx08@gmail.com>
2024-04-25 14:49:43 +0300

    Added logic for CRUD operations

--- PasteBinClone/PasteBinClone.Web/Controllers/TypeController.cs
+++ PasteBinClone/PasteBinClone.Web/Controllers/TypeController.cs
@@ -1,21 +1,117 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.Web.Interfaces;
+using PasteBinClone.Web.Models;
+using PasteBinClone.Web.Models.ViewModel;
+using PasteBinClone.Web.Request;
+using System.Collections.Generic;
 
 namespace PasteBinClone.Web.Controllers
 {
     public class TypeController : Controller
     {
-        public IActionResult Index()
+        private readonly IBaseService _baseService;
+
+        public TypeController(IBaseService baseService)
         {
-            return View();
+            _baseService = baseService;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var response = await _baseService.GetAll<ResponseAPI<IEnumerable<ContentTypeVM>>>(RouteConst.TypeRoute);
+
+            if(response != null && response.IsSuccess)
+            {
+                return View(response.Data);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult Delete()
+        [HttpPost]
+        public async Task<IActionResult> Create(ContentTypeVM typeVM)
         {
-            return View();
+            var response = await _baseService.Post<ResponseAPI<ContentTypeVM>>(typeVM,RouteConst.TypeRoute);
+
+            if(response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _baseService.GetById<ResponseAPI<ContentTypeVM>>(id, RouteConst.TypeRoute);
+
+            if(response != null && response.IsSuccess)
+            {
+                return View(response.Data);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var response = await _baseService.Delete<ResponseAPI<ContentTypeVM>>(id, RouteConst.TypeRoute);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var response = await _baseService.GetById<ResponseAPI<ContentTypeVM>>(id, RouteConst.TypeRoute);
+
+            if (response != null && response.IsSuccess)
+            {
+                return View(response.Data);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ContentTypeVM typeVM)
+        {
+            var response = await _baseService.Put<ResponseAPI<ContentTypeVM>>(typeVM, RouteConst.TypeRoute);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
