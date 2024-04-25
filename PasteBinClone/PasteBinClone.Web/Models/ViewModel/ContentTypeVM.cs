7760a6e40d7a0a50ad3f9192653d417dc7f074df
Vlad <morpexx08@gmail.com>
2024-04-25 14:50:11 +0300

    Added validation attribute

--- PasteBinClone/PasteBinClone.Web/Models/ViewModel/ContentTypeVM.cs
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/ContentTypeVM.cs
@@ -1,8 +1,12 @@
-﻿namespace PasteBinClone.Web.Models.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasteBinClone.Web.Models.ViewModel
 {
     public class ContentTypeVM
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Type field name is required.")]
         public string TypeName { get; set; }
     }
 }
