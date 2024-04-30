bb7db9c37875dec92822c7a78b96451ebfc315f6
Vlad <morpexx08@gmail.com>
2024-04-30 15:31:59 +0300

    Some changes with the repository creation logic, added regions

--- PasteBinClone/PasteBinClone.Tests/CategoryRepositoryTest/CategoryRepositoryIntegrationTest.cs
+++ PasteBinClone/PasteBinClone.Tests/CategoryRepositoryTest/CategoryRepositoryIntegrationTest.cs
@@ -14,10 +14,14 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
     public class CategoryRepositoryIntegrationTest : IDisposable
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly CategoryRepository _categoryRepository;
         public CategoryRepositoryIntegrationTest()
         {
             _dbContext = AppDbContextInMemory.GetContextInMemory();
+            _categoryRepository = new CategoryRepository(_dbContext);
         }
+
+        #region Create
 
         [Fact]
         public async Task Create_Success()
@@ -27,16 +31,13 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
             //Create a test object
             Category categoryTest = new Category { Id = 1, CategoryName = "Test1" };
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Act
 
             //Call the method with the test Category
-            var isSuccess = await categoryRepository.Create(categoryTest);
+            var isSuccess = await _categoryRepository.Create(categoryTest);
 
             //Verify that the Create method was called and the object was added to the database
-            var result = await _dbContext.Categories.FirstOrDefaultAsync(u => u.Id ==  categoryTest.Id);
+            var result = await _dbContext.Categories.FirstOrDefaultAsync(u => u.Id == categoryTest.Id);
 
             //Assert
 
@@ -47,6 +48,9 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
             Assert.NotNull(result);
             Assert.True(isSuccess);
         }
+        #endregion
+
+        #region GetById
 
         [Fact]
         public async Task GetById_Success()
@@ -57,17 +61,12 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
             Category categoryTest = new Category { Id = 1, CategoryName = "Test1" };
             int testId = 1;
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Add the test object to the database for testing the method
             _dbContext.Add(categoryTest);
             _dbContext.SaveChanges();
 
             //Act
-
-            //Call the method with the test Id
-            var result = await categoryRepository.GetById(testId);
+            var result = await _categoryRepository.GetById(testId);
 
             //Assert
 
@@ -85,21 +84,14 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
 
             //Create a test object
             Category categoryTest = new Category { Id = 1, CategoryName = "Test1" };
-
-            // Choosing an incorrect test Id
             int testId = 0;
-
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
 
             //Add the test object to the database for testing the method
             _dbContext.Add(categoryTest);
             _dbContext.SaveChanges();
 
             //Act
-
-            //Call the method with the incorrect test Id
-            var result = await categoryRepository.GetById(testId);
+            var result = await _categoryRepository.GetById(testId);
 
             //Assert
 
@@ -112,20 +104,19 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
         {
             //Arrange
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
             int testId = 1;
 
             //Act
-
-            //Call the method with the empty database
-            var result = await categoryRepository.GetById(testId);
+            var result = await _categoryRepository.GetById(testId);
 
             //Assert
 
             //Verify that the result is null since the database was empty
             Assert.Null(result);
         }
+        #endregion
+
+        #region Get
 
         [Fact]
         public async Task Get_Success()
@@ -141,13 +132,8 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
                 );
             _dbContext.SaveChanges();
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Act
-
-            //Call the method to get all objects
-            var result = await categoryRepository.Get();
+            var result = await _categoryRepository.Get();
 
             //Assert
 
@@ -163,19 +149,17 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
         {
             //Arrange
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Act
-
-            //Call the method to get all objects
-            var result = await categoryRepository.Get();
+            var result = await _categoryRepository.Get();
 
             //Assert
 
             //Verify that the number of result objects is zero
-            Assert.True(result.Count() == 0);
+            Assert.Empty(result);
         }
+        #endregion
+
+        #region Update
 
         [Fact]
         public async Task Update_Success()
@@ -189,13 +173,8 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
             _dbContext.Add(categoryTest);
             _dbContext.SaveChanges();
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Act
-
-            //Call the method to update object
-            var result = await categoryRepository.Update(new Category { Id = 1, CategoryName = "Test1" });
+            var result = await _categoryRepository.Update(new Category { Id = 1, CategoryName = "Test1" });
             var updatedCategory = await _dbContext.Categories.FirstOrDefaultAsync(u => u.Id == 1);
 
             //Assert
@@ -212,19 +191,17 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
         {
             //Arrange
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Act
-
-            //Call the method to update object
-            var result = await categoryRepository.Update(new Category { Id = 1, CategoryName = "Test1" });
+            var result = await _categoryRepository.Update(new Category { Id = 1, CategoryName = "Test1" });
 
             //Assert
 
             //Verify that the result false
             Assert.False(result);
         }
+        #endregion
+
+        #region Delete
 
         [Fact]
         public async Task Delete_Success()
@@ -238,13 +215,9 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
             _dbContext.Add(categoryTest);
             _dbContext.SaveChanges();
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
 
             //Act
-
-            //Call the method to delete object
-            var result = await categoryRepository.Delete(categoryTest.Id);
+            var result = await _categoryRepository.Delete(categoryTest.Id);
 
             //Assert
 
@@ -257,13 +230,8 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
         {
             //Arrange
 
-            //Add _dbContext options with an In-Memory database
-            var categoryRepository = new CategoryRepository(_dbContext);
-
             //Act
-
-            //Call the method to delete object
-            var result = await categoryRepository.Delete(1);
+            var result = await _categoryRepository.Delete(1);
 
             //Assert
 
@@ -271,6 +239,7 @@ namespace PasteBinClone.Tests.CategoryRepositoryTest
             Assert.False(result);
 
         }
+        #endregion
 
         private IEnumerable<Category> GetAllCategory()
         {
