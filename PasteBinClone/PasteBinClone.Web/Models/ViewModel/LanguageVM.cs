2facc0cf4e8bb22b3372de8a95bd107df327b267
Vlad <morpexx08@gmail.com>
2024-05-02 15:51:31 +0300

    Added ViewModel for Language

---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/LanguageVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasteBinClone.Web.Models.ViewModel
+{
+    public class LanguageVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The  Language field name is required.")]
+        [MaxLength(50)]
+        public string LanguageName { get; set; }
+    }
+}
