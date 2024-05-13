dc899a45c13b1be48ee18d7cc8d232d5d69d4d3b
Vlad <morpexx08@gmail.com>
2024-05-13 16:05:57 +0300

    Added user model

---
+++ PasteBinClone/PasteBinClone.Identity/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PasteBinClone.Identity.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
