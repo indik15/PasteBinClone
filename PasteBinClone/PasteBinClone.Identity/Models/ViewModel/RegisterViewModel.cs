46efd51ae9f5720511bc9e29ee98e930d0f3fc38
Vlad <morpexx08@gmail.com>
2024-05-30 19:49:34 +0300

    Added ViewModel for Login and Register

---
+++ PasteBinClone/PasteBinClone.Identity/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasteBinClone.Identity.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
