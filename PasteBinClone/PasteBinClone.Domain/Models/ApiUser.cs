140d240b9594bf08d5885421ac6436f8c160cb70
Vlad <morpexx08@gmail.com>
2024-06-05 15:57:53 +0300

    Added models for API users

---
+++ PasteBinClone/PasteBinClone.Domain/Models/ApiUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Domain.Models
+{
+    public class ApiUser
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Identity/Models/ApiUser.cs
@@ -0,0 +1,10 @@
+﻿namespace PasteBinClone.Identity.Models
+{
+    //Class to send the user to the API
+    public class ApiUser
+    {
+        public string? UserId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
