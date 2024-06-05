b07ed17e0c16084e251e178b9575d373823c35d1
Vlad <morpexx08@gmail.com>
2024-06-05 16:04:54 +0300

    Added controller for receiving users

---
+++ PasteBinClone/PasteBinClone.API/Controllers/ReceptionUserController.cs
@@ -0,0 +1,17 @@
+﻿using PasteBinClone.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.Application.ViewModels;
+
+namespace PasteBinClone.API.Controllers
+{
+    [ApiController]
+    [Route("api/receptionUser")]
+    public class ReceptionUserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> GetUser([FromBody] ApiUserViewModel apiUser)
+        {
+            return Ok(apiUser);
+        }
+    }
+}
