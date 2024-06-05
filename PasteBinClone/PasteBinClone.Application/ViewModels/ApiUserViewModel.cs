9d9b1dbcf18361d2182882e2193bc9a3701896e2
Vlad <morpexx08@gmail.com>
2024-06-05 15:59:13 +0300

    Added the view modes for the users

---
+++ PasteBinClone/PasteBinClone.Application/ViewModels/ApiUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.ViewModels
+{
+    public class ApiUserViewModel
+    {
+        public ApiUser? ApiUser { get; set; }
+        public string? ApiCode { get; set; }
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Identity/Models/ViewModel/ApiUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PasteBinClone.Identity.Models.ViewModel
+{
+    public class ApiUserViewModel
+    {
+        public ApiUser? ApiUser { get; set; }
+        public string? ApiCode { get; set; }
+    }
+}
