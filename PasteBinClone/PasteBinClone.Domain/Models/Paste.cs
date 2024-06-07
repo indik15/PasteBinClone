fc6d0740cadaffed089b6d525237cf3f36e164af
Vlad <morpexx08@gmail.com>
2024-06-07 16:44:34 +0300

    Added model for the Paste

---
+++ PasteBinClone/PasteBinClone.Domain/Models/Paste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Domain.Models
+{
+    public class Paste
+    {
+        public Guid Id { get; set; }
+        public string Title { get ; set; }
+        public string BodyUrl { get; set; }
+        public bool IsPublic { get; set; }
+        public DateTime LifeTime { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int LanguageId { get; set; }
+        public Language? Language { get; set; }
+        public int TypeId { get; set; }
+        public ContentType? Type { get; set; }
+        public string UserId {  get; set; }
+        public ApiUser? User { get; set; }
+    }
+}
