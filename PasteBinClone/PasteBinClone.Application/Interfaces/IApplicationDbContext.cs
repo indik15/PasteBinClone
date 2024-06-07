7b76069d926bbf628850259715f86413ddbbe433
Vlad <morpexx08@gmail.com>
2024-06-07 16:45:58 +0300

    Added user and paste in the IApplicationDbContext

--- PasteBinClone/PasteBinClone.Application/Interfaces/IApplicationDbContext.cs
+++ PasteBinClone/PasteBinClone.Application/Interfaces/IApplicationDbContext.cs
@@ -13,6 +13,8 @@ namespace PasteBinClone.Application.Interfaces
         DbSet<Category> Categories { get; }
         DbSet<ContentType> ContentTypes { get; }
         DbSet<Language> Languages { get; }
+        DbSet<ApiUser> ApiUsers { get; }
+        DbSet<Paste> Pastes { get; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellation = default);
     }
