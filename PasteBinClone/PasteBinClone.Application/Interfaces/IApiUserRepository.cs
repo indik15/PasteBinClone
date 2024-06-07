18fee09d27ad634fb60ec412937fb0583d924983
Vlad <morpexx08@gmail.com>
2024-06-07 16:41:39 +0300

    Added interface for the ApiUserRepository

---
+++ PasteBinClone/PasteBinClone.Application/Interfaces/IApiUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Interfaces
+{
+    public interface IApiUserRepository
+    {
+        Task<bool> GetById(string id);
+        Task<bool> Create(ApiUser user);
+        Task<bool> Update(ApiUser user);    
+    }
+}
