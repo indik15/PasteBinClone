703a93c7537f99a355a63de3507d5aa369deae53
Vlad <morpexx08@gmail.com>
2024-06-07 16:43:48 +0300

    Added configuration for user table

---
+++ PasteBinClone/PasteBinClone.Persistence/EntityConfiguration/ApiUserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Persistence.EntityConfiguration
+{
+    public class ApiUserConfiguration : IEntityTypeConfiguration<ApiUser>
+    {
+        public void Configure(EntityTypeBuilder<ApiUser> builder)
+        {
+            builder.HasKey(u => u.UserId);
+            builder.HasIndex(x => x.UserId).IsUnique();
+            builder.Property(u => u.UserId).IsRequired();
+            builder.Property(u => u.Email).IsRequired();
+            builder.Property(u => u.Name).IsRequired();
+        }
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Persistence/Migrations/20240607133258_addApiUserToDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PasteBinClone.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addApiUserToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiUsers", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiUsers_UserId",
+                table: "ApiUsers",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiUsers");
+        }
+    }
+}
