2c4c2204c3b1b4b8ad33f04dcf9a27b28074fe31
Vlad <morpexx08@gmail.com>
2024-06-07 16:53:31 +0300

    Added repository for the ApiUser

---
+++ PasteBinClone/PasteBinClone.Persistence/Repository/ApiUserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Persistence.Repository
+{
+    public class ApiUserRepository(IApplicationDbContext db) : IApiUserRepository
+    {
+        private readonly IApplicationDbContext _db = db;
+
+        public async Task<bool> Create(ApiUser user)
+        {
+            if(user == null)
+            {
+                return false;
+            }
+
+            _db.ApiUsers.Add(user);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<ApiUser> GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+
+            return await _db.ApiUsers.FirstOrDefaultAsync(u => u.UserId == id);
+        }
+
+        public async Task<bool> Update(ApiUser user)
+        {
+            //...
+
+            return false;
+        }
+    }
+}
