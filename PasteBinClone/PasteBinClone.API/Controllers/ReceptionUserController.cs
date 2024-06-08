c7578ee51945a08160e92f34f545e1cc90484b1e
Vlad <morpexx08@gmail.com>
2024-06-08 12:06:55 +0300

    Configured the controller to accept the user

--- PasteBinClone/PasteBinClone.API/Controllers/ReceptionUserController.cs
+++ PasteBinClone/PasteBinClone.API/Controllers/ReceptionUserController.cs
@@ -1,17 +1,36 @@
 ﻿using PasteBinClone.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using PasteBinClone.Application.ViewModels;
+using PasteBinClone.Application.Interfaces;
 
 namespace PasteBinClone.API.Controllers
 {
     [ApiController]
     [Route("api/receptionUser")]
-    public class ReceptionUserController : ControllerBase
+    public class ReceptionUserController(IConfiguration configuration,
+        IApiUserService userService) : ControllerBase
     {
+        private readonly IConfiguration _configuration = configuration;
+        private readonly IApiUserService _userService = userService;
+
         [HttpPost]
-        public async Task<ActionResult> GetUser([FromBody] ApiUserViewModel apiUser)
+        public async Task<ActionResult> Post([FromBody] ApiUserViewModel apiUser)
         {
-            return Ok(apiUser);
+            if(apiUser.ApiCode != _configuration["IdentityUser:SecretKey"])
+            {
+                return NotFound();
+            }
+
+            bool result = await _userService.CreateUser(apiUser.ApiUser);
+
+            if (result)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
