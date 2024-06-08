2fafffcf381392b381495b5c5206af7c63900fcb
Vlad <morpexx08@gmail.com>
2024-06-08 12:04:16 +0300

    Added service for the ApiUser

---
+++ PasteBinClone/PasteBinClone.Application/Interfaces/IApiUserService.cs
@@ -0,0 +1,17 @@
+﻿using PasteBinClone.Application.ViewModels;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Interfaces
+{
+    public interface IApiUserService
+    {
+        Task<bool> CreateUser(ApiUser user);
+        Task<ApiUser> GetApiUserById(string id);
+        Task<bool> UpdateApiUser(ApiUser user);
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Application/Services/ApiUserService.cs
@@ -0,0 +1,27 @@
+﻿using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Application.ViewModels;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Services
+{
+    public class ApiUserService(IApiUserRepository userRepository) : IApiUserService
+    {
+        private readonly IApiUserRepository _userRepository = userRepository;
+
+        public async Task<bool> CreateUser(ApiUser user) => 
+            await _userRepository.Create(user) ? true : false;
+
+        public async Task<ApiUser> GetApiUserById(string id) =>
+            await _userRepository.GetById(id);
+
+        public Task<bool> UpdateApiUser(ApiUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
