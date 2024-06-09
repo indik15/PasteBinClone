feed7a778cd8dad7d8f586ddc68eb48e67056ad2
Vlad <morpexx08@gmail.com>
2024-06-09 11:56:33 +0300

    Added DTO for the Paste

---
+++ PasteBinClone/PasteBinClone.Application/Dto/PasteDto.cs
@@ -0,0 +1,22 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Dto
+{
+    public class PasteDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsPublic { get; set; }
+        public DateTime LifeTime { get; set; }
+        public int CategoryId { get; set; }
+        public int LanguageId { get; set; }
+        public int TypeId { get; set; }
+        public string UserId { get; set; }
+    }
+}
