b4dadc8333ab67b8d257b63d2b2ed5acb3969ffb
Vlad <morpexx08@gmail.com>
2024-06-09 11:57:00 +0300

    Added view model for the Paste

---
+++ PasteBinClone/PasteBinClone.Application/ViewModels/PasteVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.ViewModels
+{
+    public class PasteVM
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsPublic { get; set; }
+        public DateTime LifeTime { get; set; }
+        public int CategoryId { get; set; }
+        public int LanguageId { get; set; }
+        public int TypeId { get; set; }
+        public string UserId { get; set; }
+    }
+}
