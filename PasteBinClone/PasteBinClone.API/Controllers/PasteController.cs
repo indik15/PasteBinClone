85e590262c55826dd08f1e3de34673c731314e72
Vlad <morpexx08@gmail.com>
2024-06-09 12:05:17 +0300

    Added controller for Paste

---
+++ PasteBinClone/PasteBinClone.API/Controllers/PasteController.cs
@@ -0,0 +1,132 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.API.Response;
+using PasteBinClone.Application.Dto;
+using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Application.ViewModels;
+using Serilog;
+
+namespace PasteBinClone.API.Controllers
+{
+    [Route("api/pastes")]
+    [ApiController]
+    public class PasteController(IPasteService pasteService,
+        IMapper mapper,
+        IValidator<PasteDto> validator) : ControllerBase
+    {
+        private readonly IPasteService _pasteService = pasteService;
+        private readonly IMapper _mapper = mapper;
+        private readonly IValidator<PasteDto> _validator = validator;
+        private readonly ResponseAPI _responseAPI = new();
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseAPI>> Post([FromBody] PasteDto pasteDto)
+        {
+            Log.Information("Request to create a Paste");
+
+            var valid = _validator.Validate(pasteDto);
+
+            if (valid.IsValid)
+            {
+                Log.Error("Validation Error: {i}", valid.Errors);
+                return ValidationProblem();
+            }
+
+            bool result = await _pasteService.CreatePaste(pasteDto);
+
+            if (result)
+            {
+                return Ok(_responseAPI);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseAPI>> Get()
+        {
+            Log.Information("Request to receive all Pastes");
+
+            IEnumerable<PasteDto> pastes = await _pasteService.GetAllPaste();
+
+            if(pastes == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var pasteVM = _mapper.Map<IEnumerable<PasteVM>>(pastes);
+                _responseAPI.Data = pasteVM;
+
+                return Ok(_responseAPI);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResponseAPI>> Get(Guid id)
+        {
+            Log.Information("Request to receive Paste by id: {@id}", id);
+
+            PasteDto pasteDto = await _pasteService.GetPasteById(id);
+
+            if (pasteDto == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var pasteVM = _mapper.Map<PasteVM>(pasteDto);
+                _responseAPI.Data = pasteVM;
+
+                return Ok(_responseAPI);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ResponseAPI>> Update([FromBody] PasteDto pasteDto)
+        {
+            Log.Information("Request to edit a Paste with id: {@id}", pasteDto.Id);
+
+            var valid = _validator.Validate(pasteDto);
+
+            if (!valid.IsValid)
+            {
+                Log.Error("Validation Error: {i}", valid.Errors);
+
+                return ValidationProblem();
+            }
+
+            bool result = await _pasteService.UpdatePaste(pasteDto);
+
+            if (!result)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(_responseAPI);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ResponseAPI>> Delete(Guid id)
+        {
+            Log.Information("Request to delete a Paste with id: {@id}", id);
+
+            bool result = await _pasteService.DeletePaste(id);
+
+            if (!result)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(_responseAPI);
+            }
+        }
+    }
+}
