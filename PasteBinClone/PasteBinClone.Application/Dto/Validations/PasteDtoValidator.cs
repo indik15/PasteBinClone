448db4354404bb34758d0c3cc85cf09b2f449300
Vlad <morpexx08@gmail.com>
2024-06-09 12:03:00 +0300

    Added validation settings for PasteDto

---
+++ PasteBinClone/PasteBinClone.Application/Dto/Validations/PasteDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Dto.Validations
+{
+    public class PasteDtoValidator : AbstractValidator<PasteDto>
+    {
+        public PasteDtoValidator()
+        {
+            RuleFor(u => u.Body).NotEmpty();
+            RuleFor(u => u.Title).NotEmpty().MaximumLength(60);
+        }
+    }
+}
