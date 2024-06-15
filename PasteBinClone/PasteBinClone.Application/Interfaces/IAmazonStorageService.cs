2c1194323f7a41b590f06caa44efbe5b4fa4faa9
Vlad <morpexx08@gmail.com>
2024-06-15 18:33:49 +0300

    Added update and delete logic

--- PasteBinClone/PasteBinClone.Application/Interfaces/IAmazonStorageService.cs
+++ PasteBinClone/PasteBinClone.Application/Interfaces/IAmazonStorageService.cs
@@ -10,5 +10,7 @@ namespace PasteBinClone.Application.Interfaces
     {
         Task<(bool, string)> UploadFile(string body);
         Task<string> GetFile(string id);
+        Task<bool> DeleteFile(string id);
+        Task<bool> UpdateFile(string id, string body);
     }
 }
--- PasteBinClone/PasteBinClone.Persistence/Services/AmazonStorageService.cs
+++ PasteBinClone/PasteBinClone.Persistence/Services/AmazonStorageService.cs
@@ -6,21 +6,69 @@ using PasteBinClone.Application.Interfaces;
 
 namespace PasteBinClone.Persistence.Services
 {
-    public class AmazonStorageService(IAmazonS3 awsClient,
-        IConfiguration configuration) : IAmazonStorageService
+    public class AmazonStorageService : IAmazonStorageService
     {
-        private readonly IAmazonS3 _awsClient = awsClient;
-        private readonly IConfiguration _configuration = configuration;
+        private readonly IAmazonS3 _awsClient;
+        private readonly IConfiguration _configuration;
+        private readonly string _bucketName;
+
+        public AmazonStorageService(IAmazonS3 awsClient,
+        IConfiguration configuration)
+        {
+            _awsClient = awsClient;
+            _configuration = configuration;
+            _bucketName = _configuration["AWS:BucketName"]!;
+        }
+        public async Task<bool> DeleteFile(string id)
+        {
+            try
+            {
+                await _awsClient.DeleteObjectAsync(_bucketName, id);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
         public async Task<string> GetFile(string id)
         {
-            string bucketName = _configuration["AWS:BucketName"];
+            try
+            {
+                var result = await _awsClient.GetObjectAsync(_bucketName, id);
 
-            var result = await _awsClient.GetObjectAsync(bucketName, id);
+                using var stream = new StreamReader(result.ResponseStream);
 
-            using var stream = new StreamReader(result.ResponseStream);
+                return await stream.ReadToEndAsync();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
 
-            return await stream.ReadToEndAsync();
+        public async Task<bool> UpdateFile(string id, string body)
+        {
+            try
+            {
+                var request = new PutObjectRequest
+                {
+                    Key = id,
+                    ContentBody = body,
+                    BucketName = _bucketName
+                };
+
+                await _awsClient.PutObjectAsync(request);
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
         }
 
         public async Task<(bool, string?)> UploadFile(string body)
@@ -28,11 +76,10 @@ namespace PasteBinClone.Persistence.Services
             try
             {
                 string id = Guid.NewGuid().ToString();
-                string bucketName = _configuration["AWS:BucketName"];
 
                 var request = new PutObjectRequest
                 {
-                    BucketName = bucketName,
+                    BucketName = _bucketName,
                     Key = id,
                     ContentBody = body,
                 };
