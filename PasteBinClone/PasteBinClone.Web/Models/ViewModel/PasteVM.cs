07a56afa1a149a9602c258552d291abc9d350c6b
Vlad <morpexx08@gmail.com>
2024-06-15 19:15:08 +0300

    Added Paste view model

---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/PasteVM.cs
@@ -0,0 +1,16 @@
+﻿namespace PasteBinClone.Web.Models.ViewModel
+{
+    public class PasteVM
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsPublic { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime ExpireAt { get; set; }
+        public int CategoryId { get; set; }
+        public int LanguageId { get; set; }
+        public int TypeId { get; set; }
+        public string UserId { get; set; }
+    }
+}
