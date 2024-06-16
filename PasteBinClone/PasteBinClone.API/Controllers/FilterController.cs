467671f500c20920bf17486edd303b02f262f00b
Vlad <morpexx08@gmail.com>
2024-06-16 18:15:41 +0300

    Added controller for filters

---
+++ PasteBinClone/PasteBinClone.API/Controllers/FilterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.API.Response;
+using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Application.Services;
+
+namespace PasteBinClone.API.Controllers
+{
+    [Route("api/filters")]
+    [ApiController]
+    public class FilterController(IFilterService filterService) : ControllerBase
+    {
+        private readonly IFilterService _filterService = filterService;
+        private readonly ResponseAPI _responseAPI = new();
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseAPI>> Get()
+        {
+            var result = await _filterService.GetAllFilters();
+            _responseAPI.Data = result;
+
+            return Ok(_responseAPI);
+        }
+    }
+}
