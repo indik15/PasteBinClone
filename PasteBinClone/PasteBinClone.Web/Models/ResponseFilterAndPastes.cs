8846a666d82df8c918b948ad6314d4f300e2d647
Vlad <morpexx08@gmail.com>
2024-06-23 12:11:41 +0300

    Model for getting all posts and filters for Home

--- PasteBinClone/PasteBinClone.Web/Models/ResponseFilterAndPastes.cs
+++ PasteBinClone/PasteBinClone.Web/Models/ResponseFilterAndPastes.cs
@@ -1,10 +1,11 @@
 ﻿using PasteBinClone.Web.Models.ViewModel;
+using PasteBinClone.Web.Models.ViewModel.Paste;
 
 namespace PasteBinClone.Web.Models
 {
     public class ResponseFilterAndPastes
     {
-        public IEnumerable<PasteVM> Pastes { get; set; }
+        public IEnumerable<HomePasteVM> Pastes { get; set; }
         public IEnumerable<CategoryVM> Categories { get; set; }
         public IEnumerable<ContentTypeVM> ContentTypes { get; set; }
         public IEnumerable<LanguageVM> Languages { get; set; }
