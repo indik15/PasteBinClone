96ffe438238cc27237497f8ea8a0c3fd865e2fe0
Vlad <morpexx08@gmail.com>
2024-06-24 11:58:52 +0300

    Added a field to display the time

--- PasteBinClone/PasteBinClone.Web/Models/ViewModel/Paste/GetPasteVM.cs
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/Paste/GetPasteVM.cs
@@ -9,9 +9,11 @@
         public DateTime CreateAt { get; set; }
         public DateTime ExpireAt { get; set; }
         public CategoryVM Category { get; set; }
-        public ContentTypeVM ContentType { get; set; }
+        public ContentTypeVM Type { get; set; }
         public LanguageVM Language { get; set; }
         public Guid UserId { get; set; }
         public string UserName { get; set; }
+
+        public TimeSpan TimeRemaining => ExpireAt - DateTime.Now;
     }
 }
