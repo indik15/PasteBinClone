5e6b6428e0cb7f790fa912b41e3c2553ce70175a
Vlad <morpexx08@gmail.com>
2024-07-05 18:28:30 +0300

    Added migrations

---
+++ PasteBinClone/PasteBinClone.Persistence/Migrations/20240705152404_addCommentsToDb.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PasteBinClone.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCommentsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    PasteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_ApiUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "ApiUsers",
+                        principalColumn: "UserId");
+                    table.ForeignKey(
+                        name: "FK_Comments_Pastes_PasteId",
+                        column: x => x.PasteId,
+                        principalTable: "Pastes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_Id",
+                table: "Comments",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PasteId",
+                table: "Comments",
+                column: "PasteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
