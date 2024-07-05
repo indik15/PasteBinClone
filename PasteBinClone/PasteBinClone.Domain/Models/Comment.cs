b8da2b5941c65a439cd13629d159e09aab3abec6
Vlad <morpexx08@gmail.com>
2024-07-05 17:23:58 +0300

    Added model for comments

---
+++ PasteBinClone/PasteBinClone.Domain/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Domain.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public string UserId { get; set; }
+        public ApiUser User { get; set; }
+        public string PasteId { get; set; }
+        public Paste Paste { get; set; }
+    }
+}
