e92c0b0478310dca5cac33ccb9ec4fabfeea2f67
Vlad <morpexx08@gmail.com>
2024-07-06 12:31:28 +0300

    Added view model for Comment

---
+++ PasteBinClone/PasteBinClone.Application/ViewModels/CommentVM.cs
@@ -0,0 +1,20 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.ViewModels
+{
+    public class CommentVM
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string? UserId { get; set; }
+        public ApiUser User { get; set; }
+        public Guid PasteId { get; set; }
+        public Paste Paste { get; set; }
+    }
+}
