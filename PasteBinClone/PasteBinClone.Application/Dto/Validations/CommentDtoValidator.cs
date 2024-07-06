624259a2a4042b5f9df0398df06c201c0b5012a5
Vlad <morpexx08@gmail.com>
2024-07-06 12:34:19 +0300

    Added validation settings for CommentDto

---
+++ PasteBinClone/PasteBinClone.Application/Dto/Validations/CommentDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Dto.Validations
+{
+    public class CommentDtoValidator : AbstractValidator<CommentDto>
+    {
+        public CommentDtoValidator()
+        {
+            RuleFor(u => u.Body).NotEmpty();
+        }
+    }
+}
