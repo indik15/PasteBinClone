e6cd697c91d58fb598ad2c6155ecccef2ce01980
Vlad <morpexx08@gmail.com>
2024-07-06 16:14:43 +0300

    Added controller for Comments

---
+++ PasteBinClone/PasteBinClone.Web/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PasteBinClone.Web.Interfaces;
+using PasteBinClone.Web.Models.ViewModel;
+using PasteBinClone.Web.Request;
+
+namespace PasteBinClone.Web.Controllers
+{
+    public class CommentController(IBaseService baseService, IUserInfo userInfo) : Controller
+    {
+        private readonly IBaseService _baseService = baseService;
+        private readonly IUserInfo _userInfo = userInfo;
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CommentVM comment)
+        {
+            string returnUrl = Request.Headers["Referer"].ToString();
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            comment.UserId = _userInfo.GetUserId(accessToken);
+
+            await _baseService.Post(comment, RouteConst.CommentRoute, accessToken);
+
+            return Redirect(returnUrl);
+        }
+    }
+}
