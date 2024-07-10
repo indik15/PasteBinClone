63b05a4a6de0b1ac378537e4340136fd7955f84f
Vlad <morpexx08@gmail.com>
2024-07-10 17:15:41 +0300

    Added a response for the comments

---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/CommentsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PasteBinClone.Web.Models.ViewModel
+{
+    public class CommentsResponse
+    {
+        public IEnumerable<CommentVM> CommentVMs { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
