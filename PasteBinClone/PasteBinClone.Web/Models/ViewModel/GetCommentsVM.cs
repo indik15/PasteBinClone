638a6caddd9303cf7c466766e3a70904b816bbfb
Vlad <morpexx08@gmail.com>
2024-07-10 17:17:21 +0300

    Added properties for comment pages

--- PasteBinClone/PasteBinClone.Web/Models/ViewModel/GetCommentsVM.cs
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/GetCommentsVM.cs
@@ -4,5 +4,10 @@
     {
         public IEnumerable<CommentVM> Comments { get; set; }
         public string ReturnUrl { get; set; }
+        public int PageNumber { get; set; }
+        public string PasteId { get; set; }
+        public bool IsActiveRightArrow { get; set; }
+        public bool IsActiveLeftArrow { get; set; }
+
     }
 }
