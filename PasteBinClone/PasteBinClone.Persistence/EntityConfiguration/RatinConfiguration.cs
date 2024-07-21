ab5cb14884db3f4b7bf068d0ddb504be6e8be7df
Vlad <morpexx08@gmail.com>
2024-07-21 12:40:16 +0300

    Added configurations for Rating

---
+++ PasteBinClone/PasteBinClone.Persistence/EntityConfiguration/RatinConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Persistence.EntityConfiguration
+{
+    public class RatinConfiguration : IEntityTypeConfiguration<Rating>
+    {
+        public void Configure(EntityTypeBuilder<Rating> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.HasIndex(u => u.Id);
+        }
+    }
+}
