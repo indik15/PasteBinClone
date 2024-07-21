1c775c85ae1397b3b9850870776775ac1d91251f
Vlad <morpexx08@gmail.com>
2024-07-21 12:39:02 +0300

    Added model for paste rating

---
+++ PasteBinClone/PasteBinClone.Domain/Models/Rating.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Domain.Models
+{
+    public class Rating
+    {
+        public Guid Id { get; set; }
+        public bool IsLiked { get; set; }
+        public bool IsDisliked { get; set; }
+        public string UserId { get; set; }
+        public ApiUser ApiUser { get; set; }
+        public Guid PasteId { get; set; }
+        public Paste Paste { get; set; }
+    }
+}
