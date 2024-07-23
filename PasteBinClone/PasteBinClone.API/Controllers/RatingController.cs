927532790b157cbd31f21bd44492a348e012744c
Vlad <morpexx08@gmail.com>
2024-07-23 11:46:03 +0300

    Added controller for Rating

---
+++ PasteBinClone/PasteBinClone.API/Controllers/RatingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.API.Response;
+using PasteBinClone.Application.Dto;
+using PasteBinClone.Application.Interfaces;
+
+namespace PasteBinClone.API.Controllers
+{
+    [ApiController]
+    [Route("api/rating")]
+    public class RatingController(IRatingService ratingService) : ControllerBase
+    {
+        private readonly IRatingService _ratingService = ratingService;
+        private readonly ResponseAPI response = new();
+
+        [HttpPut]
+        public async Task<ActionResult<ResponseAPI>> Put([FromBody] RatingDto ratingDto)
+        {
+            bool result = await _ratingService.UpdatePasteRating(ratingDto);
+
+            if (result)
+            {
+                return Ok(response);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Web/Controllers/RatingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.Web.Interfaces;
+using PasteBinClone.Web.Models.ViewModel;
+using PasteBinClone.Web.Request;
+
+namespace PasteBinClone.Web.Controllers
+{
+    public class RatingController(IBaseService baseService) : Controller
+    {
+        private readonly IBaseService _baseService = baseService;
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RatingVM rating)
+        {
+            string returnUrl = Request.Headers["Referer"].ToString();
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var response = await _baseService.Put(rating, RouteConst.RatingRoute, accessToken);
+
+            if(response != null && response.IsSuccess)
+            {
+                return Redirect(returnUrl);
+            }
+
+            return NotFound();
+        }
+    }
+}
