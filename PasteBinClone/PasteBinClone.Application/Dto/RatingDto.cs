afc2a0652b10d69aea824a14b04586b678f96d67
Vlad <morpexx08@gmail.com>
2024-07-23 11:44:14 +0300

    Added DTO for Rating

---
+++ PasteBinClone/PasteBinClone.Application/Dto/RatingDto.cs
@@ -0,0 +1,17 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Dto
+{
+    public class RatingDto
+    {
+        public bool IsLiked { get; set; }
+        public bool IsDisliked { get; set; }
+        public string? UserId { get; set; }
+        public Guid PasteId { get; set; }
+    }
+}
