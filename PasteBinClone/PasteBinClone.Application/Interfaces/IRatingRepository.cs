e98493458e0ac79ea0cdd626db00f5c6f7fe0d35
Vlad <morpexx08@gmail.com>
2024-07-23 10:32:05 +0300

    Added a repository for Rating

---
+++ PasteBinClone/PasteBinClone.Application/Interfaces/IRatingRepository.cs
@@ -0,0 +1,15 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Interfaces
+{
+    public interface IRatingRepository
+    {
+        Task<bool> Create(Rating rating);
+        Task<bool> Update(Rating rating);
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Persistence/Repository/RatingRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Persistence.Repository
+{
+    public class RatingRepository(IApplicationDbContext db) : IRatingRepository
+    {
+        private readonly IApplicationDbContext _db = db;
+
+        public async Task<bool> Create(Rating rating)
+        {
+            if(rating == null)
+            {
+                return false;
+            }
+
+            _db.Ratings.Add(rating);
+
+            Paste paste = _db.Pastes.FirstOrDefault(u => u.Id == rating.PasteId);
+
+            if(paste == null)
+            {
+                return false;
+            }
+
+            if (rating.IsLiked)
+                paste.Likes++;
+
+            else if (rating.IsDisliked)
+                paste.Dislikes++;
+
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Update(Rating rating)
+        {
+            if(rating == null)
+            {
+                return false;
+            }
+
+            Rating? currentRating = await _db.Ratings
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.UserId == rating.UserId && u.PasteId == u.PasteId);
+
+            Paste paste = await _db.Pastes.FirstOrDefaultAsync(u => u.Id == rating.PasteId);
+
+            if(rating.IsLiked && currentRating.IsDisliked)
+            {
+                paste.Likes++;
+                paste.Dislikes--;
+            }
+            else if (rating.IsDisliked && currentRating.IsLiked)
+            {
+                paste.Likes--;
+                paste.Dislikes++;
+            }
+            else if (rating.IsDisliked && currentRating.IsDisliked)
+            {
+                paste.Dislikes--;
+            }
+            else if (rating.IsLiked && currentRating.IsLiked)
+            {
+                paste.Likes--;
+            }
+
+            _db.Ratings.Update(rating);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
