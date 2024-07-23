cce34a9e7769f725deefd02821b0cd2257989ff5
Vlad <morpexx08@gmail.com>
2024-07-23 11:46:55 +0300

    Added view model for Rating

---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/RatingVM.cs
@@ -0,0 +1,10 @@
+﻿namespace PasteBinClone.Web.Models.ViewModel
+{
+    public class RatingVM
+    {
+        public bool IsLiked { get; set; }
+        public bool IsDisliked { get; set; }
+        public string? UserId { get; set; }
+        public Guid PasteId { get; set; }
+    }
+}
