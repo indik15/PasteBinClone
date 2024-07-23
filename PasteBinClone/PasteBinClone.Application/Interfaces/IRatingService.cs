8746301b386090a49bbbb37c95ef0823feafa0f4
Vlad <morpexx08@gmail.com>
2024-07-23 11:43:31 +0300

    Added service for Rating

---
+++ PasteBinClone/PasteBinClone.Application/Interfaces/IRatingService.cs
@@ -0,0 +1,15 @@
+﻿using PasteBinClone.Application.Dto;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Interfaces
+{
+    public interface IRatingService
+    {
+        Task<bool> UpdatePasteRating(RatingDto rating);
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Application/Services/RatingService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using PasteBinClone.Application.Dto;
+using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.Services
+{
+    public class RatingService(IRatingRepository ratingRepository, IMapper mapper) : IRatingService
+    {
+        private readonly IRatingRepository _ratingRepository = ratingRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<bool> UpdatePasteRating(RatingDto ratingDto)
+        {
+            Rating rating = _mapper.Map<Rating>(ratingDto);
+
+            return await _ratingRepository.Update(rating);
+        }
+    }
+}
