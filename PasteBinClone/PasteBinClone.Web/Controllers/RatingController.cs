a28e65832dcbb44f99a43e5701c41bdeeeb86e41
Vlad <morpexx08@gmail.com>
2024-07-26 12:33:18 +0300

    Fixed logic of sending user Id

--- PasteBinClone/PasteBinClone.Web/Controllers/RatingController.cs
+++ PasteBinClone/PasteBinClone.Web/Controllers/RatingController.cs
@@ -6,16 +6,19 @@ using PasteBinClone.Web.Request;
 
 namespace PasteBinClone.Web.Controllers
 {
-    public class RatingController(IBaseService baseService) : Controller
+    public class RatingController(IBaseService baseService, IUserInfo userInfo) : Controller
     {
         private readonly IBaseService _baseService = baseService;
+        private readonly IUserInfo _userInfo = userInfo;
 
         [HttpPost]
         public async Task<IActionResult> Create(RatingVM rating)
-        {
+        {               
             string returnUrl = Request.Headers["Referer"].ToString();
 
             var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            rating.UserId = _userInfo.GetUserId(accessToken);
 
             var response = await _baseService.Put(rating, RouteConst.RatingRoute, accessToken);
 
