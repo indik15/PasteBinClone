2307a6ff2bc7647c0e47b7c09b70064236600eeb
Vlad <morpexx08@gmail.com>
2024-07-28 15:40:09 +0300

    Added Likes property

--- PasteBinClone/PasteBinClone.Application/Dto/HomePasteDto.cs
+++ PasteBinClone/PasteBinClone.Application/Dto/HomePasteDto.cs
@@ -12,6 +12,7 @@ namespace PasteBinClone.Application.Dto
         public Guid Id { get; set; }
         public string Title { get; set; }
         public bool IsPublic { get; set; }
+        public ulong Likes { get; set; }
         public DateTime CreateAt { get; set; }
         public DateTime ExpireAt { get; set; }
         public CategoryVM Category { get; set; }
--- PasteBinClone/PasteBinClone.Web/Models/ViewModel/Paste/HomePasteVM.cs
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/Paste/HomePasteVM.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public bool IsPublic { get; set; }
+        public ulong Likes { get; set; }
         public DateTime CreateAt { get; set; }
         public DateTime ExpireAt { get; set; }
         public CategoryVM Category { get; set; }
