093bd14acef7fad4287ebba68435793decb1f425
Vlad <morpexx08@gmail.com>
2024-07-31 19:47:53 +0300

    Added a method to get all user Pastes

--- PasteBinClone/PasteBinClone.Web/Controllers/ProfileController.cs
+++ PasteBinClone/PasteBinClone.Web/Controllers/ProfileController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PasteBinClone.Web.Interfaces;
+using PasteBinClone.Web.Models;
+using PasteBinClone.Web.Models.ViewModel;
+using PasteBinClone.Web.Request;
+using PasteBinClone.Web.Services;
 
 namespace PasteBinClone.Web.Controllers
 {
-    public class ProfileController : Controller
+    public class ProfileController(IBaseService baseService,IUserInfo userInfo) : Controller
     {
+        private readonly IBaseService _baseService = baseService;
+        private readonly IUserInfo _userInfo = userInfo;
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +23,36 @@ namespace PasteBinClone.Web.Controllers
         public IActionResult Edit()
         {
             return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> UserPastes(int pageNumber = 1)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            string userId = _userInfo.GetUserId(accessToken);
+
+            var response = await _baseService.GetById(userId, RouteConst.ProfileRoute, accessToken, obj: pageNumber);
+
+            if (response != null && response.IsSuccess)
+            {
+                ResponsePaste userPastes = JsonConvert.DeserializeObject<ResponsePaste>(response.Data.ToString());
+
+                UserPastesVM pastesVM = new UserPastesVM
+                {
+                    PasteVMs = userPastes.Pastes,
+                    PageNumber = pageNumber,
+                    IsActiveRightArrow = userPastes.TotalPages > pageNumber ? true : false,
+                    IsActiveLeftArrow = pageNumber > 1 ? true : false
+                };
+
+                return View(pastesVM);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
