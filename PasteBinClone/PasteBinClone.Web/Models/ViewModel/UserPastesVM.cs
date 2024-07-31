22deb197bc8f7fe5e19cbeb700fd015cdcfb8830
Vlad <morpexx08@gmail.com>
2024-07-31 19:46:04 +0300

    Added view for UserPaste page

---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/UserPastesVM.cs
@@ -0,0 +1,12 @@
+﻿using PasteBinClone.Web.Models.ViewModel.Paste;
+
+namespace PasteBinClone.Web.Models.ViewModel
+{
+    public class UserPastesVM
+    {
+        public int PageNumber { get; set; }
+        public bool IsActiveRightArrow { get; set; }
+        public bool IsActiveLeftArrow { get; set; }
+        public IEnumerable<HomePasteVM> PasteVMs { get; set; }
+    }
+}
