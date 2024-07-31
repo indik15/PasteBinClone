3a2604a09924b72165b7c422a7e023d0d50fa57f
Vlad <morpexx08@gmail.com>
2024-07-31 16:39:38 +0300

    Added controller to get "Top rated paste" and "All user pastes"

---
+++ PasteBinClone/PasteBinClone.API/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasteBinClone.API.Response;
+using PasteBinClone.Application.Dto;
+using PasteBinClone.Application.Interfaces;
+using PasteBinClone.Application.ViewModels;
+using System.Collections.Generic;
+
+namespace PasteBinClone.API.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController(IPasteService pasteService, IMapper mapper) : ControllerBase
+    {
+        private readonly IPasteService _pasteService = pasteService;
+        private readonly IMapper _mapper = mapper;
+        private readonly ResponseAPI _response = new();
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseAPI>> Get()
+        {
+            IEnumerable<HomePasteDto> pasteDtos = await _pasteService.GetTopRatedPastes();
+
+            if(pasteDtos == null)
+            {
+                return NotFound();
+            }
+
+            _response.Data = _mapper.Map<IEnumerable<HomePasteVM>>(pasteDtos);
+
+            return Ok(_response);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<ResponseAPI>> Get(string userId)
+        {
+            IEnumerable<HomePasteDto> pasteDtos = await _pasteService.GetAllUserPastes(userId);
+
+            if (pasteDtos == null)
+            {
+                return NotFound();
+            }
+
+            _response.Data = _mapper.Map<IEnumerable<HomePasteVM>>(pasteDtos);
+
+            return Ok(_response);
+        }
+    }
+}
