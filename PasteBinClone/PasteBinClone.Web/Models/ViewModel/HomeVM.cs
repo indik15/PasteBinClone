25e40e87c64bb959e23d08a30e56b24df9d1d69b
Vlad <morpexx08@gmail.com>
2024-07-31 16:43:49 +0300

    Added UserPasteVMs and TopRatedPasteVMs properties

--- PasteBinClone/PasteBinClone.Web/Models/ViewModel/HomeVM.cs
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/HomeVM.cs
@@ -9,6 +9,8 @@ namespace PasteBinClone.Web.Models.ViewModel
         public bool IsActiveRightArrow { get; set; }
         public bool IsActiveLeftArrow { get; set; }
         public IEnumerable<HomePasteVM> PasteVMs { get; set; }
+        public IEnumerable<HomePasteVM> UserPasteVMs { get; set; }
+        public IEnumerable<HomePasteVM> TopRatedPasteVMs { get; set; }
         public IEnumerable<SelectListItem> Categories { get; set; }
         public IEnumerable<SelectListItem> ContentTypes { get; set; }
         public IEnumerable<SelectListItem> Languages { get; set; }
