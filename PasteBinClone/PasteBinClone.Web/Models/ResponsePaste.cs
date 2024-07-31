02d1736bb9a32cf2b3af94f01e73382f7b5ac981
Vlad <morpexx08@gmail.com>
2024-07-31 19:46:47 +0300

    Added response model for Paste

---
+++ PasteBinClone/PasteBinClone.Web/Models/ResponsePaste.cs
@@ -0,0 +1,10 @@
+﻿using PasteBinClone.Web.Models.ViewModel.Paste;
+
+namespace PasteBinClone.Web.Models
+{
+    public class ResponsePaste
+    {
+        public IEnumerable<HomePasteVM> Pastes { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
