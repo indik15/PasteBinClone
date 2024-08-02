c88ea5000fd2263a7860c50903c6f5ae98a27022
Vlad <morpexx08@gmail.com>
2024-08-02 15:45:50 +0300

    Added response model for recaptcha

---
+++ PasteBinClone/PasteBinClone.Identity/Models/RecaptchaResponse.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace PasteBinClone.Identity.Models
+{
+    public class RecaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool IsSuccess { get; set; }
+        [JsonProperty("error-codes")]
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
