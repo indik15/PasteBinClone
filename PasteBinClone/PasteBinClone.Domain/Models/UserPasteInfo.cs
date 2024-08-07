89dff8097b5c468fd3ba67c05d72a1c96c624af6
Vlad <morpexx08@gmail.com>
2024-08-07 10:46:59 +0300

    Added a class to display user statistics

---
+++ PasteBinClone/PasteBinClone.Domain/Models/UserPasteInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Domain.Models
+{
+    public class UserPasteInfo
+    {
+        public Guid Id { get; set; }
+        public int TotalPastes { get; set; }
+        public int TotalPublicPastes { get; set; }
+        public int TotalPrivatePastes { get; set; }
+        public int TotalActivePastes { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/Paste/UserPasteInfoVM.cs
@@ -0,0 +1,14 @@
+﻿namespace PasteBinClone.Web.Models.ViewModel.Paste
+{
+    public class UserPasteInfoVM
+    {
+        public Guid Id { get; set; }
+        public int TotalPastes { get; set; }
+        public int TotalPublicPastes { get; set; }
+        public int TotalPrivatePastes { get; set; }
+        public int TotalActivePastes { get; set; }
+
+        public string UserId { get; set; }
+        public ApiUserVM ApiUser { get; set; }
+    }
+}
