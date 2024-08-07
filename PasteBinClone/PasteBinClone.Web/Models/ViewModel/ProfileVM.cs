4742b129fc33d9b7330c21425d84037b60b239a2
Vlad <morpexx08@gmail.com>
2024-08-07 10:54:21 +0300

    Added a view model for the main profile page

---
+++ PasteBinClone/PasteBinClone.Web/Models/ViewModel/ProfileVM.cs
@@ -0,0 +1,10 @@
+﻿using PasteBinClone.Web.Models.ViewModel.Paste;
+
+namespace PasteBinClone.Web.Models.ViewModel
+{
+    public class ProfileVM
+    {
+        public ApiUserVM ApiUser { get; set; }
+        public IEnumerable<HomePasteVM> Pastes { get; set; }
+    }
+}
