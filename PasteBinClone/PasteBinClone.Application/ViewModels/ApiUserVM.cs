df2bad76ea1f5123150150f28c54c16e9fde56a6
Vlad <morpexx08@gmail.com>
2024-08-07 10:48:21 +0300

    Added view model for ApiUser

---
+++ PasteBinClone/PasteBinClone.Application/ViewModels/ApiUserVM.cs
@@ -0,0 +1,20 @@
+﻿using PasteBinClone.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBinClone.Application.ViewModels
+{
+    public class ApiUserVM
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? City { get; set; }
+        public string Email { get; set; }
+        public UserPasteInfo? UserPasteInfo { get; set; }
+    }
+}
